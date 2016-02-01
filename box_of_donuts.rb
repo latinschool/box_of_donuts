ebb6ae708f334d5f2fe1677cbf5f30dcba014e5f
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-01 07:56:53 -0600

    simple donut exercise, commented instruction

---
+++ box_of_donuts.rb
@@ -0,0 +1,16 @@
+donuts = ["boston creme", "boston creme", "old fashioned", "old fashioned", "long john", "long john", "strawberry", "stawberry", "maple", "double chocolate cake", "double chocolate cake", "some french thing?"]
+
+#How many donuts are in this box?
+
+
+#How can you know what the 4th donut in the box is?
+
+#How can you assign donuts in this box to classmates (think of classmates as variables)
+
+#Find a method that lets you search the box of donuts to see if yours in there
+
+#If you took a donut from this array, what method would you use to completely remove it from the array..err, box
+
+#How could you add another donut to this box?
+
+#How can you iterate through the box and show a list of all the donuts?
